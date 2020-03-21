ba589f4faae2ec4296f1e3da0f2efcf64ccbb847
Mardoniosc <Mardonio@live.com>
2020-03-21 17:22:42 -0300

    First commit

---
+++ src/test/java/br/com/mardonio/foster/FosterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mardonio.foster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
