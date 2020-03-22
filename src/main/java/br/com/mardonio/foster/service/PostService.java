b7f3537897079d0b68135af2fbdd148fb02d8fd4
Mardoniosc <Mardonio@live.com>
2020-03-22 13:18:33 -0300

    :v: Deletando um Post com DELETE

--- src/main/java/br/com/mardonio/foster/service/PostService.java
+++ src/main/java/br/com/mardonio/foster/service/PostService.java
@@ -3,10 +3,12 @@ package br.com.mardonio.foster.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.mardonio.foster.domain.Post;
 import br.com.mardonio.foster.repository.PostRepository;
+import br.com.mardonio.foster.service.exceptions.DataIntegrityException;
 import br.com.mardonio.foster.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,14 @@ public class PostService {
 		find(obj.getId());
 		
 		return postRepository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			postRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir o post");
+		}
 	}
 }
